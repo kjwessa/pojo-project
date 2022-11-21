023abce6e7ae86e7d3f10e2fe6482a91bddd5e29
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2022-11-21 16:53:34 -0600

    Completed 05

--- practice-for-week-02-pojo-main/problems/05-does-key-exist.js
+++ practice-for-week-02-pojo-main/problems/05-does-key-exist.js
@@ -11,8 +11,18 @@ doesKeyExist(obj1, 'name'); // => false
 ***********************************************************************/
 
 function doesKeyExist(obj, key) {
-  // Your code here
+  if (obj.hasOwnProperty(key)) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+doesKeyExist(obj1, 'course'); // => true
+doesKeyExist(obj1, 'name'); // => false
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = doesKeyExist;
